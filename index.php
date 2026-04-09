4267b5b783d7b050932127310aab575c1e8fd70b
lray138 <57813289+lray138@users.noreply.github.com>
2026-04-09 14:57:35 -0400

    still tweaking

--- index.php
+++ index.php
@@ -87,6 +87,22 @@
     if ($bp_og_canonical_url !== '') {
         $bp_og_canonical_url = $bp_normalize_og_image_url($bp_og_canonical_url);
     }
+
+    $bp_og_published = '';
+    $bp_og_modified = '';
+    if (is_singular()) {
+        $post = get_queried_object();
+        if ($post instanceof WP_Post && $post->post_status === 'publish') {
+            $bp_ts_pub = get_post_time('U', true, $post);
+            $bp_ts_mod = get_post_modified_time('U', true, $post);
+            if ($bp_ts_pub) {
+                $bp_og_published = gmdate('c', (int) $bp_ts_pub);
+            }
+            if ($bp_ts_mod) {
+                $bp_og_modified = gmdate('c', (int) $bp_ts_mod);
+            }
+        }
+    }
     ?>
     <?php wp_head(); ?>
     <?php if ($bp_og_canonical_url !== '') : ?>
@@ -95,6 +111,16 @@
     <meta property="og:title" content="<?php echo esc_attr(wp_strip_all_tags((string) $page_title)); ?>">
     <?php if (is_front_page()) : ?>
     <meta property="og:type" content="website">
+    <?php elseif (is_singular('post')) : ?>
+    <meta property="og:type" content="article">
+    <?php elseif (is_singular()) : ?>
+    <meta property="og:type" content="website">
+    <?php endif; ?>
+    <?php if ($bp_og_published !== '') : ?>
+    <meta property="article:published_time" content="<?php echo esc_attr($bp_og_published); ?>">
+    <?php endif; ?>
+    <?php if ($bp_og_modified !== '') : ?>
+    <meta property="article:modified_time" content="<?php echo esc_attr($bp_og_modified); ?>">
     <?php endif; ?>
     <?php if ($bp_home_og_description !== '') : ?>
     <meta property="og:description" content="<?php echo esc_attr($bp_home_og_description); ?>">
