567a79128471f6afe3471bacce035c72ffb8241f
lray138 <57813289+lray138@users.noreply.github.com>
2026-04-09 10:59:27 -0400

    adding cloudflare temp like this

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/theme.css">
     <script src="https://unpkg.com/htmx.org@1.9.12"></script>
     <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+    <?php if (is_page('contact')) : ?>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+    <?php endif; ?>
 </head>
 <?php
     $can_edit = is_user_logged_in() && current_user_can('edit_post', get_the_ID());
